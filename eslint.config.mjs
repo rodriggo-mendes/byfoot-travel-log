91b6ffe70ecd6105111ba9585c28feeb474d24dd
Rodriggo <rodriggo.loiola@gmail.com>
2025-08-26 22:13:05 -0300

    deps: setup eslint and editor setup

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import antfu from '@antfu/eslint-config';
+
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt(antfu(
+  {
+    vue: true,
+    typescript: true,
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: 'single',
+    },
+  },
+  {
+    rules: {
+      'ts/consistent-type-definitions': ['error', 'type'],
+      'no-console': ['warn'],
+      'node/no-process-env': ['off'],
+      'perfectionist/sort-imports': ['error', {
+        tsconfigRootDir: '.',
+      }],
+      'unicorn/filename-case': ['error', {
+        cases: {
+          kebabCase: true,
+          snakeCase: true,
+        },
+        ignore: ['README.md'],
+      }],
+    },
+  },
+));
